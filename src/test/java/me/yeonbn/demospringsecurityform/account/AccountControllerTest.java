2b0ea0f9197b9251db91ff3129f3aade77adf6b7
yeonbn <yeonbn88@gmail.com>
2020-01-17 01:57:38 +0900

    [SECURITY] mock test #2, with annotation and custom annotation

--- src/test/java/me/yeonbn/demospringsecurityform/account/AccountControllerTest.java
+++ src/test/java/me/yeonbn/demospringsecurityform/account/AccountControllerTest.java
@@ -5,12 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,36 +23,41 @@ public class AccountControllerTest {
     MockMvc mockMvc;
 
     @Test
+    @WithAnonymousUser
     public void index_anonymous() throws Exception {
-        mockMvc.perform(get("/").with(anonymous()))
+        mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithTestUser
     public void index_user() throws Exception {
-        mockMvc.perform(get("/").with(user("test").roles("USER")))
+        mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
     public void index_admin() throws Exception {
-        mockMvc.perform(get("/").with(user("admin").roles("ADMIN")))
+        mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithTestUser
     public void admin_user() throws Exception {
-        mockMvc.perform(get("/admin").with(user("test").roles("USER")))
+        mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
 
     @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
     public void admin_admin() throws Exception {
-        mockMvc.perform(get("/admin").with(user("admin").roles("ADMIN")))
+        mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
---
+++ src/test/java/me/yeonbn/demospringsecurityform/account/WithTestUser.java
@@ -0,0 +1,11 @@
+package me.yeonbn.demospringsecurityform.account;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "test", roles = "USER")
+public @interface WithTestUser {
+}
