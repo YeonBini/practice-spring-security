f567b1e4a42137e04092ec20145ed25a3d34f799
yeonbn <yeonbn88@gmail.com>
2020-01-17 01:32:04 +0900

    [SECURITY] customize password encoder

--- src/main/java/me/yeonbn/demospringsecurityform/SpringSecurityForm.java
+++ src/main/java/me/yeonbn/demospringsecurityform/SpringSecurityForm.java
@@ -2,6 +2,14 @@ package me.yeonbn.demospringsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpringSecurityForm {
@@ -10,4 +18,16 @@ public class SpringSecurityForm {
         SpringApplication.run(SpringSecurityForm.class, args);
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        // customize encoder
+        String idForEncode = "bcrypt";
+        Map encoders = new HashMap<>();
+        encoders.put(idForEncode, new BCryptPasswordEncoder());
+        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+        return new DelegatingPasswordEncoder(idForEncode, encoders);
+    }
 }
