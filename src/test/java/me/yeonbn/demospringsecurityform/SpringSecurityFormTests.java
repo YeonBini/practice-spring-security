f00032f8067bb710c66012ad29a593e46a16f190
yeonbn <yeonbn88@gmail.com>
2020-01-16 23:25:42 +0900

    project setting

---
+++ src/test/java/me/yeonbn/demospringsecurityform/SpringSecurityFormTests.java
@@ -0,0 +1,13 @@
+package me.yeonbn.demospringsecurityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFormTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
